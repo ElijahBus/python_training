c9a883de8008b643eb23cb948fcd999e1bcd9e86
Elijah Busanga <elijahbus.ny@gmail.com>
2021-02-03 23:27:51 +0200

    Varianle usage

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello world")
+age = 20
+age = 30
+
+price = 99.50
+first_name = "Elijah"
+is_online = True
+
+patient_name = "John Smith"
+age = 20
+is_new_patient = True
+
+print(age, first_name, is_online)
