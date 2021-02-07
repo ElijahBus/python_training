36acbd3486b0f704e35b59e214e285f608d43156
Elijah Busanga <elijahbus.ny@gmail.com>
2021-02-07 17:00:44 +0200

    Exception handing in python

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-# def greet_user(first_name, last_name):
-#   print(f"Hello {first_name} {last_name}")
-#   print("Greet from function")
-
-# print("Start")
-# greet_user(last_name="John", first_name="Smith")
-# print("Finish")
-
-def square(number):
-  print(number * number)
-
-print('start')
-print(square(3))
-print('finish')
+try:
+  age = int(input('Age: '));
+  income = 1000 / age
+  print(age)
+except ValueError:
+  print('Invalid value')
+except ZeroDivisionError:
+  print('Age should not be equal to 0')
